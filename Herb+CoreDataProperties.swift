6d0b0e7d4dd906e4d46dfc10248cda0c7b687262
eric mead <ermead@vt.edu>
2015-11-02 19:52:22 -0700

    kinda got something working but still sucks

---
+++ Herb+CoreDataProperties.swift
@@ -0,0 +1,27 @@
+//
+//  Herb+CoreDataProperties.swift
+//  TraditionalMedicineCapstoneProject
+//
+//  Created by Eric Mead on 11/2/15.
+//  Copyright © 2015 Eric Mead. All rights reserved.
+//
+//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
+//  to delete and recreate this implementation file for your updated model.
+//
+
+import Foundation
+import CoreData
+
+extension Herb {
+
+    @NSManaged var botanicalName: String?
+    @NSManaged var category: String?
+    @NSManaged var englishName: String?
+    @NSManaged var imageName: String?
+    @NSManaged var majorFormulas: String?
+    @NSManaged var meridians: String?
+    @NSManaged var pinyinName: String?
+    @NSManaged var temp: String?
+    @NSManaged var uses: String?
+
+}
---
+++ Herb.swift
@@ -0,0 +1,16 @@
+//
+//  Herb.swift
+//  TraditionalMedicineCapstoneProject
+//
+//  Created by Eric Mead on 11/2/15.
+//  Copyright © 2015 Eric Mead. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class Herb: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
