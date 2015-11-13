0413b6fa052cacbcbddcd1a9b8e05134a6e3c8c8
eric mead <ermead@vt.edu>
2015-11-12 22:59:40 -0700

    buttons working

---
+++ TraditionalMedicineCapstoneProject/CombinationsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CombinationsTableViewCell.swift
+//  TraditionalMedicineCapstoneProject
+//
+//  Created by Eric Mead on 11/12/15.
+//  Copyright © 2015 Eric Mead. All rights reserved.
+//
+
+import UIKit
+
+class CombinationsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
