7849dd539b38a920ef8764e1dafd400d6224d8dc
eric mead <ermead@vt.edu>
2015-11-14 18:43:53 -0700

    eh

---
+++ TraditionalMedicineCapstoneProject/HerbSets.swift
@@ -0,0 +1,66 @@
+//
+//  DefaultHerbs.swift
+//  TraditionalMedicineCapstoneProject
+//
+//  Created by Eric Mead on 11/14/15.
+//  Copyright © 2015 Eric Mead. All rights reserved.
+//
+
+import Foundation
+
+
+class HerbSets {
+
+    static let sharedController = HerbSets()
+    
+    var HerbSet = HerbSetType.ReleaseWindCold
+    var herbset: [Herb] = []
+    
+    var thisHerbSetType: HerbSetType?
+    
+    enum HerbSetType {
+        case AllHerbs
+        case ReleaseWindCold
+        case ReleaseWindHeat
+        
+    }
+
+    func getHerbSet(thisSetType: HerbSetType) -> [Herb]{
+        //self.thisHerbSetType = thisSetType
+        self.HerbSet = thisSetType
+        print("this herb set I am trying to load is \(HerbSet)")
+        return setHerbSet()
+    }
+
+    
+    
+   
+    
+    func setHerbSet()->[Herb]{
+        
+        let cinnamon = Herb(pinyinName: "Gui Zhi", botanicalName: "insert botanical name", englishName: "Cinnamon", category: "Release the Exterior", temp: "Warm", meridians: "insert meridians", uses: HerbUses.sharedController.getHerbUse(HerbUses.Herb.GuiZhi), majorFormulas: "Gui Zhi Tang", imageId1: nil, imageId2: nil)
+        let ginseng = Herb(pinyinName: "Ren Shen", botanicalName: "insert botanical name", englishName: "Ginseng", category: "Tonify the Qi", temp: "Warm", meridians: "insert meridians", uses: HerbUses.sharedController.getHerbUse(HerbUses.Herb.RenShen), majorFormulas: "Si Jun Zi Tang", imageId1: nil, imageId2: nil)
+        let dangGui = Herb(pinyinName: "Dang Gui", botanicalName: "insert botanical name", englishName: "Angelica", category: "Tonify the Blood", temp: "Warm", meridians: "insert meridians", uses:HerbUses.sharedController.getHerbUse(HerbUses.Herb.BaiZhu), majorFormulas: "Si Wu Tang", imageId1: nil, imageId2: nil)
+        
+        let baishao = Herb(pinyinName: "Bai Shao", botanicalName: "Radix Peony", englishName: "White Peony", category: "Yin Tonic", temp: "Cool", meridians: "LR", uses: HerbUses.sharedController.getHerbUse(HerbUses.Herb.BaiShao), majorFormulas: "Xiao Yao San", imageId1: nil, imageId2: nil)
+        
+        
+        switch HerbSet {
+            
+        case .AllHerbs:
+            
+            return [cinnamon, ginseng]
+            
+        case .ReleaseWindCold:
+            
+            return [cinnamon, ginseng, dangGui]
+        
+            
+        case .ReleaseWindHeat:
+            
+            return [cinnamon]
+            
+        }
+    }
+    
+}
---
+++ TraditionalMedicineCapstoneProject/HerbUses.swift
@@ -0,0 +1,55 @@
+//
+//  HerbUses.swift
+//  TraditionalMedicineCapstoneProject
+//
+//  Created by Eric Mead on 11/14/15.
+//  Copyright © 2015 Eric Mead. All rights reserved.
+//
+
+import Foundation
+
+class HerbUses: NSObject {
+    
+    static let sharedController = HerbUses()
+    
+    var thisHerb: Herb? = nil
+    var needUse: Bool? = true
+    var use: String?
+    
+    enum Herb {
+        case BaiShao,GuiZhi,RenShen,BaiZhu
+    }
+    
+    func getHerbUse(herb: HerbUses.Herb) -> String {
+        
+        let UseForHerb = herb
+        
+        switch UseForHerb {
+            
+        case .BaiShao   : self.use = baishaoUse
+        case .GuiZhi    : self.use = guizhiUse
+        case .RenShen   : self.use = renshenUse
+        case .BaiZhu    : self.use = baizhuUse
+        default : self.use = "default"
+        }
+       
+        
+        return self.use!
+    }
+
+    
+    var baishaoUse = "This is a test to see what gets entered and where"
+    var guizhiUse = "This is the test for guizhiUse"
+    var renshenUse = "This is a test for ren shen use"
+    var baizhuUse = "Another test for bai zhu use now"
+    
+
+}
+
+
+
+
+
+
+
+
