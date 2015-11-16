1472987d467838014ff44cdb818808593e9f8bc5
eric mead <ermead@vt.edu>
2015-11-16 10:12:52 -0700

    buttons getting better

--- OtherResourcesViewController.swift
+++ OtherResourcesViewController.swift
@@ -13,22 +13,17 @@ class OtherResourcesViewController: UIViewController {
     var titleA: String = "TEST"
 
     @IBOutlet weak var button1Outlet: UIButton!
-    
     @IBOutlet weak var button2Outlet: UIButton!
-    
     @IBOutlet weak var button3Outlet: UIButton!
-    
     @IBOutlet weak var button4Outlet: UIButton!
-    
     @IBOutlet weak var button5Outlet: UIButton!
-    
     @IBOutlet weak var button6Outlet: UIButton!
-    
     @IBOutlet weak var button7Outlet: UIButton!
-    
     @IBOutlet weak var button8Outlet: UIButton!
-    
     @IBOutlet weak var button9Outlet: UIButton!
+    
+    
+    var arrayOfButtonNames: [String] = []
     
     @IBAction func buttonedTapped(sender: UIButton){
         print("button \(sender.titleLabel!.text) got tapped")
@@ -65,10 +60,24 @@ class OtherResourcesViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        setUpButtons()
         // Do any additional setup after loading the view.
     }
 
+    func setUpButtons() {
+        arrayOfButtonNames = ["Yoga", "Test", "Another", "Another2", "Yoga", "Test", "Another", "Another2", "Yoga", "Test"]
+        
+            self.button1Outlet.setTitle(arrayOfButtonNames[0], forState: .Normal)
+            self.button2Outlet.setTitle(arrayOfButtonNames[1], forState: .Normal)
+            self.button3Outlet.setTitle(arrayOfButtonNames[2], forState: .Normal)
+            self.button4Outlet.setTitle(arrayOfButtonNames[3], forState: .Normal)
+            self.button5Outlet.setTitle(arrayOfButtonNames[4], forState: .Normal)
+            self.button6Outlet.setTitle(arrayOfButtonNames[5], forState: .Normal)
+            self.button7Outlet.setTitle(arrayOfButtonNames[6], forState: .Normal)
+            self.button8Outlet.setTitle(arrayOfButtonNames[7], forState: .Normal)
+            self.button9Outlet.setTitle(arrayOfButtonNames[8], forState: .Normal)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
