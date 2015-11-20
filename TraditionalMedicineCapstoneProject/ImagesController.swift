60cf012c5ab835576a8c25f494fd6bc24d822987
eric mead <ermead@vt.edu>
2015-11-19 17:50:24 -0700

    eh, not really working right

---
+++ TraditionalMedicineCapstoneProject/ImagesController.swift
@@ -0,0 +1,13 @@
+//
+//  ImagesController.swift
+//  TraditionalMedicineCapstoneProject
+//
+//  Created by Eric Mead on 11/19/15.
+//  Copyright © 2015 Eric Mead. All rights reserved.
+//
+
+import UIKit
+
+class ImagesController: NSObject {
+
+}
