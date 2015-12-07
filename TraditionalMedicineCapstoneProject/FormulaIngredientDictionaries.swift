a2fe8a7ff2fcbee1aa1d3750b95a784fa5af7880
eric mead <ermead@vt.edu>
2015-12-06 22:51:03 -0700

    added Formula Default Data

---
+++ TraditionalMedicineCapstoneProject/FormulaIngredientDictionaries.swift
@@ -0,0 +1,9 @@
+//
+//  FormulaIngredientDictionaries.swift
+//  TraditionalMedicineCapstoneProject
+//
+//  Created by Eric Mead on 12/6/15.
+//  Copyright © 2015 Eric Mead. All rights reserved.
+//
+
+import Foundation
---
+++ TraditionalMedicineCapstoneProject/FormulaSets.swift
@@ -0,0 +1,9 @@
+//
+//  FormulaSets.swift
+//  TraditionalMedicineCapstoneProject
+//
+//  Created by Eric Mead on 12/6/15.
+//  Copyright © 2015 Eric Mead. All rights reserved.
+//
+
+import Foundation
